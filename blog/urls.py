c074f64c8374acb492df5ccd4eedeaf0b147b6e0
maximo-cobeaga <maximocobeaga@gmail.com>
2022-03-31 19:13:16 -0300

    actualizacion

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
+from unicodedata import name
 from venv import create
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView, BlogDatailView, BlogUpdateView
 
 app_name="blog"
 
 urlpatterns= [
     path('', BlogListView.as_view(), name='home'),
-    path('create/', BlogCreateView.as_view(), name='create')
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('<int:pk>', BlogDatailView.as_view(), name='detail'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,21 @@
+from dataclasses import field
 from multiprocessing import context
+from re import template
 from turtle import title
-from django.shortcuts import redirect, render
+from django import views
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
-from django.views.generic import View
+from django.views.generic import View, UpdateView
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy
+
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
         context={   
+            'posts': posts
         }
         return render(request, 'blog_list.html', context)
 
@@ -35,3 +42,20 @@ class BlogCreateView(View):
             
         }
         return render(request, 'blog_create.html', context)
+
+class BlogDatailView(View):
+    def get(self, request, pk,*args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        context={
+           'post':post 
+        }
+        return render(request, 'blog_detail.html', context)
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+
+    def get_succes_url(self): 
+        pk = self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs = {'pk':pk} )
